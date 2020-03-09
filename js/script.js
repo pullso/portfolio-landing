776124c9fb6c35c47d06e93b6fead53ca22ae001
Pavel Pullso <p.dubitskiy@ya.ru>
2020-03-09 19:16:26 +0500

    add progress bar js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 new WOW().init()
 
-$(window).scroll(function() {
+const progress = document.querySelector('.progress')
+
+$(window).scroll(function(e) {
   if ($(this).scrollTop() != 0) $('#toTop').fadeIn()
   else $('#toTop').fadeOut()
+  let windowScroll = document.body.scrollTop || document.documentElement.scrollTop
+  let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+  let per = (windowScroll / windowHeight) * 100
+  console.log(windowScroll, windowHeight, per)
+  progress.style.width = per + '%'
 })
 
 $('#toTop').click(function() {
