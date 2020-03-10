471f6b34c280308941967f38364f205fd8422631
Pavel Pullso <p.dubitskiy@ya.ru>
2020-03-10 12:14:57 +0500

    add yandex metric

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-new WOW().init()
+window.onload = function(params) {
+  new WOW().init()
 
-const progress = document.querySelector('.progress')
+  const progress = document.querySelector('.progress')
 
-$(window).scroll(function(e) {
-  if ($(this).scrollTop() != 0) $('#toTop').fadeIn()
-  else $('#toTop').fadeOut()
-  let windowScroll = document.body.scrollTop || document.documentElement.scrollTop
-  let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
-  let per = (windowScroll / windowHeight) * 100
-  console.log(windowScroll, windowHeight, per)
-  progress.style.width = per + '%'
-})
+  $(window).scroll(function(e) {
+    if ($(this).scrollTop() != 0) $('#toTop').fadeIn()
+    else $('#toTop').fadeOut()
+    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop
+    let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    let per = (windowScroll / windowHeight) * 100
+    console.log(windowScroll, windowHeight, per)
+    progress.style.width = per + '%'
+  })
 
-$('#toTop').click(function() {
-  $('body,html').animate(
-    {
-      scrollTop: 0,
-    },
-    800,
-  )
-})
+  $('#toTop').click(function() {
+    $('body,html').animate(
+      {
+        scrollTop: 0,
+      },
+      800,
+    )
+  })
+}
