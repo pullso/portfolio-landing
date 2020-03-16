cdc520efa9eb2117f927b83be37b4c40ffa474f7
Pavel Pullso <p.dubitskiy@ya.ru>
2020-03-16 22:55:24 +0500

    fix header and add 2 works

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
-window.onload = function(params) {
-  new WOW().init()
+new WOW().init();
 
-  const progress = document.querySelector('.progress')
+window.onload = function(params) {
+  const progress = document.querySelector(".progress");
 
   $(window).scroll(function(e) {
-    if ($(this).scrollTop() != 0) $('#toTop').fadeIn()
-    else $('#toTop').fadeOut()
-    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop
-    let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
-    let per = (windowScroll / windowHeight) * 100
-    console.log(windowScroll, windowHeight, per)
-    progress.style.width = per + '%'
-  })
+    if ($(this).scrollTop() != 0) $("#toTop").fadeIn();
+    else $("#toTop").fadeOut();
+    let windowScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    let windowHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let per = (windowScroll / windowHeight) * 100;
+    console.log(windowScroll, windowHeight, per);
+    progress.style.width = per + "%";
+  });
 
-  $('#toTop').click(function() {
-    $('body,html').animate(
+  $("#toTop").click(function() {
+    $("body,html").animate(
       {
-        scrollTop: 0,
+        scrollTop: 0
       },
-      800,
-    )
-  })
-}
+      800
+    );
+  });
+};
